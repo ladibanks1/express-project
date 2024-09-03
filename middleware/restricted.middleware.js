24191fa6634389c31ead80ddcf4ea11f862ae98f
Oladipupo <oladipupobankole24@gmail.com>
2024-09-03 16:58:29 +0100

    updated the access time

--- middleware/restricted.middleware.js
+++ middleware/restricted.middleware.js
@@ -3,7 +3,7 @@ export default function restrictWorkingHours(req, res, next) {
     const dayOfWeek = now.getDay(); 
     const hourOfDay = now.getHours(); 
 
-    if (dayOfWeek >= 1 && dayOfWeek <= 5 && hourOfDay >= 9 && hourOfDay < 16) {
+    if (dayOfWeek >= 1 && dayOfWeek <= 5 && hourOfDay >= 9 && hourOfDay < 17) {
         next(); // Allow access
     } else {
         res.status(403).send('Access is allowed only during working hours (Monday to Friday, from 9 AM to 5 PM).');
